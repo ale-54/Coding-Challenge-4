c513c81fd66c26bf249f889bbe53a745bc32643e
Ashley Le <ashleyle@usf.edu>
2025-02-02 17:19:08 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -6,3 +6,12 @@ if (purchaseAmount > 100) {
     console.log(`Discount Applied to Purchase`);
 }; //applied a 15% discount to purchases over $100
 console.log(`Final Amount after Discount: $${finalAmount}`); //680 in this example
+
+//Task 2: For Loop
+let sales = [60, 100, 200, 140, 43]; //sales figures
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}; //calculating total sales (all 5 listed)
+console.log(`Sales Total: $${totalSales}`); //the display for total sales
+
